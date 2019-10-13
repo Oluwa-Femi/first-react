b91420e37bbbed9c68db1cd306acb8b68626c7ab
Elvis Onobo <elvis.onobo@gmail.com>
2019-10-13 05:07:24 +0100

    added conditional styling to the question and punch line props

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -4,8 +4,9 @@ import '../style.css'
 function Joke(props){
     return(
         <div>
-            <p>Question: {props.jokes.question}</p>
-            <p>Punch Line: {props.jokes.punchline}</p>
+            <p style={{display: props.jokes.question ? "block" : "none"}}>Question: {props.jokes.question}</p>
+            <p style={{color: !props.jokes.question && "red"}}>Punch Line: {props.jokes.punchline}</p>
+            <hr />
         </div>
     )
 }
