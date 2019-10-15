afc633c86ec47ee23cb50c7a383711f15d0eade2
Elvis Onobo <elvis.onobo@gmail.com>
2019-10-15 20:21:48 +0100

    added JokesData array to mimick dynamic content; and mapping the data to be returned

---
+++ src/JokesData.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const jokesData = [
+    {
+        id: 1,
+        punchLine: 'cross the bridge',
+        question: 'how do you cross a bridge?'
+    },
+    {
+        id: 2,
+        punchLine: 'Because some people are too dark skinned',
+        question: 'why does the sun shine?'
+    },
+    {
+        id: 3,
+        punchLine: 'Just some ass hole',
+        question: 'Who am I?'
+    }
+] 
+
+export default jokesData
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -4,7 +4,7 @@ import '../style.css'
 function ContactCard(props){
     return(
         <div className="">
-            <img src={props.contact.imgURL} />
+            <img src={props.contact.imgURL} alt="" />
             <h3>{props.contact.name}</h3>
             <p>Phone: {props.contact.phone}</p>
             <p>E-mail: {props.contact.email}</p>
